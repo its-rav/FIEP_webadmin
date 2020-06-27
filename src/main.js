c4e391ad88111f4e1aee2ed56dabbdca722e993a
Nhân Võ <NhanVTSE130478@fpt.edu.vn>
2020-06-28 02:04:43 +0700

    Project initialization

---
+++ src/main.js
@@ -0,0 +1,48 @@
+/*
+ =========================================================
+ * Vue Black Dashboard - v1.1.0
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/black-dashboard
+ * Copyright 2018 Creative Tim (http://www.creative-tim.com)
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
+import Vue from "vue";
+import VueRouter from "vue-router";
+import RouterPrefetch from "vue-router-prefetch";
+import App from "./App";
+
+import firebase from "firebase/app";
+import config from "./config";
+const firebaseConfig = config.firebaseConfig;
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faFontAwesome } from "@fortawesome/free-brands-svg-icons";
+library.add(faFontAwesome);
+// register component
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+// TIP: change to import router from "./router/starterRouter"; to start with a clean layout
+import router from "./router/index";
+
+import BlackDashboard from "./plugins/blackDashboard";
+import i18n from "./i18n";
+import "./registerServiceWorker";
+
+Vue.use(BlackDashboard);
+Vue.use(VueRouter);
+Vue.use(RouterPrefetch);
+
+firebase.initializeApp(firebaseConfig);
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  i18n,
+  render: h => h(App)
+}).$mount("#app");
