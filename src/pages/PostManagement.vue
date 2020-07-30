2b535675cade372dafc21b4229f2c7ff30012f27
quocbinh341999 <binhtqse130088@fpt.edu.vn>
2020-07-31 03:54:55 +0700

    Update Search

--- src/pages/PostManagement.vue
+++ src/pages/PostManagement.vue
@@ -79,7 +79,7 @@
       </el-table-column>
       <el-table-column align="right">
         <template slot="header" slot-scope="scope">
-          <el-input  v-model="search" v-on:change="onSearchInput($event)" size="mini" placeholder="Type to search" />
+          <el-input  v-model="search" v-on:change="onSearchInput(search)" size="mini" placeholder="Type to search" />
         </template>
         <template slot-scope="scope">
           <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
@@ -103,7 +103,7 @@
         </template>
       </el-table-column>
     </el-table>
-    <el-row style="margin-top: 10px" v-if="searchResult == null">
+    <el-row style="margin-top: 10px" v-if="searchResult == null || this.rsPage === 1">
       <el-col :span="6" :offset="11">
         <el-button
           v-for="item in pagination"
@@ -152,6 +152,7 @@ export default {
       pagination: [],
       totalPages: 0,
       searchResult: null,
+      rsPage: 0,
     };
   },
   created: function() {
@@ -192,7 +193,11 @@ export default {
       new PostRepository(req)
         .get({ pageSize, pageNumber })
         .then(rs => {
-          this.tableData = rs.data.data;
+         if(this.searchResult === null){
+            this.tableData = rs.data.data;
+          }else{
+            this.searchResult = rs.data.data;
+          }
           console.log(this.tableData);
         })
         .catch(e => console.error(e));
@@ -297,6 +302,11 @@ export default {
       this.tableData.splice(index, 1);
     },
     async onSearchInput(e){
+      if(this.search === ""){
+        this.rsPage = 1
+      }else{
+        this.rsPage = 0
+      }
       try {
         let result=await axios.get(`${backendIp}/api/posts?query=${e}`);
       console.log(result);
